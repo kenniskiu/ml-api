40d1cb3209abb4f06cc27b23920f1556a0c12d31
kenniskiu <kenniskiufk123@gmail.com>
2022-02-25 12:12:23 +0700

    commit

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 import uvicorn 
 from fastapi import FastAPI
 import joblib,os
-gender_vectorizer = open("models/gender_vectorizer.pkl","rb")
+gender_vectorizer = open("models/gender_vectorizer.pkl","rb") 
+#will open gender vectorizer model, that uses NLP to get the root of the words. check point(1) in the documents for content of pkl.
 gender_cv = joblib.load(gender_vectorizer)
+# joblib is to provide lightweight pipelining.
 gender_nv_model = open("models/gender_nv_model.pkl","rb")
+#will open gender nv model, that will predict the name's gender.
 gender_clf = joblib.load(gender_nv_model)
+
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"text":'Hello API bepp'}
-
-
-@app.get('/items/{name}')
-async def get_items(name):
-    return {"name":name}
-
-@app.get('/predict/{name}')
-async def predict(name):
-    vectorized_name = gender_cv.transform([name]).toarray()
+@app.get('/predict/{name}') #the process will be held in localhost:PORT/predict/"whatever name is inputted"
+async def predict(name): # name of the process
+    vectorized_name = gender_cv.transform([name]).toarray() 
+    #the name will be put into the array and through some process the string will be cleaned from punctations and whitespaces
+    #then it will be put into the list of models
     prediction = gender_clf.predict(vectorized_name)
+    #will predict the name
     if prediction[0] == 0:
         result = 'female'   
     else:
